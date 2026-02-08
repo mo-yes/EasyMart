811030d972076a36fcf6c1de777e449c05a95729
mo-yes <mohamedyasser490216@gmail.com>
2026-02-08 14:12:59 +0200

    fix proxy export for next 16

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { getToken } from 'next-auth/jwt'
 import { NextResponse , NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
 const cookieName =
   process.env.NODE_ENV === "production"
     ? "__Secure-next-auth.session-token" 
